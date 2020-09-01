6d1110a18bd0ab9a512f1fcfe60fdb5359c1fa3e
Chris Latham <backwardspy@gmail.com>
2020-09-01 22:14:12 +0100

    add bouncing title text

---
+++ ui/menu/TitleBob.gd
@@ -0,0 +1,9 @@
+extends HBoxContainer
+
+onready var letters := get_children()
+
+func _process(delta: float):
+    var t := OS.get_system_time_msecs() / 1000.0
+    for i in get_child_count():
+        var letter: TextureRect = letters[i]
+        letter.rect_position.y = -abs(sin(t - i * 0.2)) * 12.0
