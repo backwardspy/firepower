85abb52f0e57737981c54778e4d8dbfb45103bd2
Chris Latham <backwardspy@gmail.com>
2020-09-06 16:25:27 +0100

    add about screen with credits

--- states/Menu.gd
+++ states/Menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 onready var _tween := $Tween
 
-func start_game():
+func fade_to_scene(path: String):
     focus_mode = Control.FOCUS_NONE
 
     $Blackout.visible = true
@@ -29,9 +29,15 @@ func start_game():
 
     _tween.start()
     yield(_tween, "tween_all_completed")
-    var err = get_tree().change_scene("res://states/Game.tscn")
+    var err = get_tree().change_scene(path)
     if err:
-        push_error("failed to load game scene: %s" % err)
+        push_error("failed to load scene %s: %s" % [path, err])
+
+func start_game():
+    fade_to_scene("res://states/Game.tscn")
+
+func about():
+    fade_to_scene("res://states/About.tscn")
 
 func quit_game():
     get_tree().quit()
